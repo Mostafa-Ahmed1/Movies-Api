ed876e7bc52a2bc63765f74bcd71261354dc57c5
Mostafa Ahmed <80547961+Mostafa-Ahmed1@users.noreply.github.com>
2026-08-07 05:57:33 +0300

    Replace AutoMapper with explicit DTO mapping

--- Helpers/MappingProfile.cs
+++ Helpers/MappingProfile.cs
@@ -1,13 +1,19 @@
-﻿
-namespace MoviesAPI.Helpers
+namespace MoviesAPI.Helpers;
+
+public static class DtoMappings
 {
-    public class MappingProfile : Profile
+    public static MovieDetailsDto ToDetailsDto(this Movie movie)
     {
-        public MappingProfile()
+        return new MovieDetailsDto
         {
-            CreateMap<Movie, MovieDetailsDto>();
-            CreateMap<MovieDto, Movie>()
-                .ForMember(src => src.Poster, opt => opt.Ignore());
-        }
+            Id = movie.Id,
+            Title = movie.Title,
+            Year = movie.Year,
+            Rate = movie.Rate,
+            Storeline = movie.Storeline,
+            Poster = movie.Poster,
+            GenreId = movie.GenreId,
+            GenreName = movie.Genre?.Name ?? string.Empty
+        };
     }
 }
